b53c378623c6795d50fe3a0054d163cee720a2e6
James Dullaghan <james@fldlvl.com>
2017-08-31 14:42:54 -0600

    Update voe request xml

---
+++ lib/equifax/worknumber/voe/researched/reverify.rb
@@ -0,0 +1,42 @@
+module Equifax
+  module Worknumber
+    module VOE
+      class Reverify < ::Equifax::Worknumber::Base
+        def self.call(opts)
+          voe = Equifax::Worknumber::VOE::Reverify.new(opts)
+
+          Equifax::Client.request(
+            voe.send(:url),
+            { request_method: :post },
+            voe.send(:request_params),
+          )
+        end
+
+        private
+
+        def xml
+          <<-eos
+            <REQUEST_GROUP>
+              <SUBMITTING_PARTY _Name="#{vendor_id}" />
+              <REQUEST LoginAccountIdentifier="#{account_number}" LoginAccountPassword="#{password}">
+                <KEY _Name="EMSOrderNumber" _Value="#{order_number}" />
+                <REQUEST_DATA>
+                  <VOI_REQUEST LenderCaseIdentifier="#{lender_case_id}">
+                    <VOI_REQUEST_DATA
+                      VOIReportTypeOtherDescription="RVVOE"
+                      VOIReportRequestActionType="Other"
+                      VOIReportRequestActionTypeOtherDescription="Reverify" />
+                    <LOAN_APPLICATION>
+                      <BORROWER _FirstName="#{first_name}" _LastName="#{last_name}" _SSN="#{ssn}" _PrintPositionType="Borrower">
+                      </BORROWER>
+                    </LOAN_APPLICATION>
+                  </VOI_REQUEST>
+                </REQUEST_DATA>
+              </REQUEST>
+            </REQUEST_GROUP>
+          eos
+        end
+      end
+    end
+  end
+end
