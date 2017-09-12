3dbdedc0592720bda212ab5afa4e7462e778d80f
James Dullaghan <james@fldlvl.com>
2017-09-12 12:44:58 -0600

    Modify voi reverify class

--- lib/equifax/worknumber/voi/researched/reverify.rb
+++ lib/equifax/worknumber/voi/researched/reverify.rb
@@ -1,40 +1,42 @@
 module Equifax
   module Worknumber
     module VOI
-      class Reverify < ::Equifax::Worknumber::Base
-        def self.call(opts)
-          voe = Equifax::Worknumber::VOI::Reverify.new(opts)
+      module Researched
+        class Reverify < ::Equifax::Worknumber::Base
+          def self.call(opts)
+            voe = Equifax::Worknumber::VOI::Researched::Reverify.new(opts)
 
-          Equifax::Client.request(
-            voe.send(:url),
-            { request_method: :post },
-            voe.send(:request_params),
-          )
-        end
+            Equifax::Client.request(
+              voe.send(:url),
+              { request_method: :post },
+              voe.send(:request_params),
+            )
+          end
 
-        private
+          private
 
-        def xml
-          <<-eos
-            <?xml version="1.0" encoding="utf-8"?>
-            <REQUEST_GROUP>
-              <SUBMITTING_PARTY _Name="#{vendor_id}" />
-              <REQUEST LoginAccountIdentifier="#{account_number}" LoginAccountPassword="#{password}">
-                <KEY _Name="EMSOrderNumber" _Value="#{order_number}" />
-                <REQUEST_DATA>
-                  <VOI_REQUEST LenderCaseIdentifier="#{lender_case_id}">
-                    <VOI_REQUEST_DATA
-                      VOIReportTypeOtherDescription="RVVOI"
-                      VOIReportRequestActionType="Other"
-                      VOIReportRequestActionTypeOtherDescription="Reverify" />
-                    <LOAN_APPLICATION>
-                      <BORROWER _FirstName="#{first_name}" _LastName="#{last_name}" _SSN="#{ssn}" _PrintPositionType="Borrower"></BORROWER>
-                    </LOAN_APPLICATION>
-                  </VOI_REQUEST>
-                </REQUEST_DATA>
-              </REQUEST>
-            </REQUEST_GROUP>
-          eos
+          def xml
+            <<-eos
+              <?xml version="1.0" encoding="utf-8"?>
+              <REQUEST_GROUP>
+                <SUBMITTING_PARTY _Name="#{vendor_id}" />
+                <REQUEST LoginAccountIdentifier="#{account_number}" LoginAccountPassword="#{password}">
+                  <KEY _Name="EMSOrderNumber" _Value="#{order_number}" />
+                  <REQUEST_DATA>
+                    <VOI_REQUEST LenderCaseIdentifier="#{lender_case_id}">
+                      <VOI_REQUEST_DATA
+                        VOIReportTypeOtherDescription="RVVOI"
+                        VOIReportRequestActionType="Other"
+                        VOIReportRequestActionTypeOtherDescription="Reverify" />
+                      <LOAN_APPLICATION>
+                        <BORROWER _FirstName="#{first_name}" _LastName="#{last_name}" _SSN="#{ssn}" _PrintPositionType="Borrower"></BORROWER>
+                      </LOAN_APPLICATION>
+                    </VOI_REQUEST>
+                  </REQUEST_DATA>
+                </REQUEST>
+              </REQUEST_GROUP>
+            eos
+          end
         end
       end
     end
